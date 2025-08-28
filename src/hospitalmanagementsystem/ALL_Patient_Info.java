c8bb3892a54f7b390f841300cf5a1635b856d284
mayur sonwane <mayurperks@gmail.com>
2025-08-29 01:49:53 +0530

    patientinfooooo

---
+++ src/hospitalmanagementsystem/ALL_Patient_Info.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalmanagementsystem;
+import javax.swing.*;
+import java.awt.*;
+import java.sql.ResultSet;
+import net.proteanit.sql.DbUtils;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ *
+ * @author Dell
+ */
+public class ALL_Patient_Info extends JFrame{
+    ALL_Patient_Info(){
+        
+        JPanel panel = new JPanel();
+        panel.setBounds(5, 5, 890, 590);
+        panel.setBackground(new Color(90,156,163));
+        panel.setLayout(null);
+        add(panel);
+        
+        JTable table = new JTable();
+        table.setBounds(10,40,900,450);
+        table.setBackground(new Color(98,156,163));
+        table.setFont(new Font("Tahoma",Font.BOLD,14));
+        panel.add(table);
+        
+        try{
+            conn c = new conn();
+            String q = "select * from patient_info";
+            ResultSet resultSet=c.statement.executeQuery(q);
+            table.setModel(DbUtils.resultSetToTableModel(resultSet));
+                    
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        JLabel label1 = new JLabel("ID");
+        label1.setBounds(31, 11, 100, 14);
+        label1.setFont(new Font("Tahoma",Font.BOLD,14));
+        panel.add(label1);
+        
+        JLabel label2 = new JLabel("Number");
+        label2.setBounds(150, 11, 130, 14);
+        label2.setFont(new Font("Tahoma",Font.BOLD,14));
+        panel.add(label2);
+        
+        JLabel label3 = new JLabel("Name");
+        label3.setBounds(270, 11, 70, 14);
+        label3.setFont(new Font("Tahoma",Font.BOLD,14));
+        panel.add(label3);  
+        
+       
+        
+        JLabel label4 = new JLabel("Gender");
+        label4.setBounds(350, 9, 100, 14);
+        label4.setFont(new Font("Tahoma",Font.BOLD,14));
+        panel.add(label4);
+        
+        JLabel label5 = new JLabel("Disease");
+        label5.setBounds(460, 9, 100, 14);
+        label5.setFont(new Font("Tahoma",Font.BOLD,14));
+        panel.add(label5);
+        
+        JLabel label6 = new JLabel("Room");
+        label6.setBounds( 570, 9, 70, 14);
+        label6.setFont(new Font("Tahoma",Font.BOLD,14));
+        panel.add(label6);
+        
+        JLabel label7= new JLabel("Time");
+        label7.setBounds(720, 9, 130, 14);
+        label7.setFont(new Font("Tahoma",Font.BOLD,14));
+        panel.add(label7);
+        
+        JLabel label8 = new JLabel("Deposits");
+        label8.setBounds(800, 11, 100, 14);
+        label8.setFont(new Font("Tahoma",Font.BOLD,14));
+        panel.add(label8);
+        
+        JButton button = new JButton("Back"); 
+        button.setBounds(350,500,120,30);
+        button.setBackground(Color.BLACK);
+        button.setForeground(Color.white);
+        panel.add(button);
+        button.addActionListener(new ActionListener(){
+                  @Override
+            public void actionPerformed(ActionEvent e) {
+      setVisible(false);
+            }
+        }
+        
+        
+        );
+                
+        setUndecorated(true);
+        
+        setSize(900,600);
+        setLayout(null);
+        setLocation(300,200);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        new ALL_Patient_Info();
+    }
+}
